351766b24a1f607df4f2b67ea0a5700d9da41145
afoxxvi <afoxxvi@outlook.com>
2024-07-04 18:06:30 +0800

    Migrate to 1.19.4

--- Fabric/src/main/java/com/afoxxvi/asteorbar/overlay/FabricGuiRegistry.java
+++ Fabric/src/main/java/com/afoxxvi/asteorbar/overlay/FabricGuiRegistry.java
@@ -2,7 +2,6 @@ package com.afoxxvi.asteorbar.overlay;
 
 import com.afoxxvi.asteorbar.overlay.parts.BaseOverlay;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.afoxxvi.asteorbar.overlay.parts.OriginsOverlay;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 
@@ -19,7 +18,6 @@ public class FabricGuiRegistry {
         REGISTRY.add(Overlays.AIR_LEVEL);
         REGISTRY.add(Overlays.EXPERIENCE_BAR);
         REGISTRY.add(Overlays.ARMOR_LEVEL);
-        REGISTRY.add(new OriginsOverlay());
         REGISTRY.add(Overlays.STRING);
 
     }
